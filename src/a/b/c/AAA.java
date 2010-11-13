01f8d4c64da7d56252e3103b0f8ede9b46dbd2df
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2010-11-13 16:59:11 +0300

    First commit

---
+++ src/a/b/c/AAA.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright IrsCo (c) 2010.
+ */
+
+package a.b.c;
+
+import com.sun.xml.internal.fastinfoset.util.StringArray;
+
+/**
+ * by Irina.Petrovskaya, on 09.11.10, 13:23
+ */
+public class AAA {
+    private StringArray arr;
+}
