d435aae16b54bc37151a9aa7711ef436fec11528
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2010-11-15 18:13:16 +0300

    add Dummy

---
+++ src/d/e/f/Dummy.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright IrsCo (c) 2010.
+ */
+package d.e.f;
+
+import java.util.Iterator;
+
+/**
+ * by Irina.Petrovskaya, on 15.11.10, 17:07
+ */
+public abstract class Dummy {
+    /**
+     *  some dummy comment
+     **/
+    public static String boo(){
+                  return "boo";
+    }
+}
