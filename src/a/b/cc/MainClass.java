ed12e8769e9fb2449a85af79ab328c19fc83869c
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-02-26 18:05:01 +0300

    edit AAA.java, add MainClass.java

---
+++ src/a/b/cc/MainClass.java
@@ -0,0 +1,23 @@
+package a.b.cc;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 2/26/2018.
+ * Project: uml_empty
+ * *******************************
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+    }
+}
