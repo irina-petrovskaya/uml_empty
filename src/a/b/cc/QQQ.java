5757dadc3319dfdaea0f088cda68778116ea33d6
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-05-28 13:45:31 +0300

    QQQ.java

---
+++ src/a/b/cc/QQQ.java
@@ -0,0 +1,10 @@
+package a.b.cc;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 5/28/2018.
+ * Project: uml_empty_iai
+ * *******************************
+ */
+public class QQQ {
+}
