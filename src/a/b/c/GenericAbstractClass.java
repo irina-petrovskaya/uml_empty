25e3665e35749d1454f240cc1f6b7979a7469502
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2024-01-10 14:52:30 +0100

    change GenericAbstractClass.java

--- src/a/b/c/GenericAbstractClass.java
+++ src/a/b/c/GenericAbstractClass.java
@@ -3,4 +3,19 @@ package a.b.c;
 
 public abstract class GenericAbstractClass<B extends GenericAbstractClass<B,C>, C extends SomeClass>
 {
+    private final B b;
+    private final C c;
+
+    public GenericAbstractClass(B b, C c) {
+        this.b = b;
+        this.c = c;
+    }
+
+    public B getB() {
+        return b;
+    }
+
+    public C getC() {
+        return c;
+    }
 }
