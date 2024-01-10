06f26f54f888c04244a3575397f1e7591f7f984c
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2024-01-10 14:50:11 +0100

    more

---
+++ src/a/b/c/GenericAbstractClass.java
@@ -0,0 +1,6 @@
+package a.b.c;
+
+
+public abstract class GenericAbstractClass<B extends GenericAbstractClass<B,C>, C extends SomeClass>
+{
+}
---
+++ src/a/b/c/SomeClass.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *
+ * <p>Created by irina on 1/10/2024.</p>
+ * <p>Project: uml_empty</p>
+ * *
+ */
+public class SomeClass extends AnotherClass{
+}
